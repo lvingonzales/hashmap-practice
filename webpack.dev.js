fdee043ae4b22e456a1d92ebdc67c638be4ae520
lvingonzales <lvingonzales15@gmail.com>
2025-01-01 13:56:47 -0500

    fix corrupt packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,11 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-<<<<<<< HEAD
-  entry: "./src/main.js",
-=======
   entry: "./src/index.js",
->>>>>>> release-1.0
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
